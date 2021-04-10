2fd562b2cca9d2aff37eaa33332b64805c067d39
Anders Grasdal <WobblyMusicStand@gmail.com>
2021-04-09 21:03:46 -0700

    Integrating Icon Support

--- update-title.js
+++ update-title.js
@@ -11,13 +11,34 @@ Hooks.on("init", function() {
 	  type: String,
 	  onChange: updateTitle, // A callback function which triggers when the setting is changed
 	});	
+	
+	game.settings.register("update-title", "pageicon", {
+	  name: "Page Icon",
+	  hint: "The icon that appears on the browser tab",
+	  scope: "world",     // This specifies a client-stored setting
+	  config: true,        // This specifies that the setting appears in the configuration view
+	  default: "icons/vtt.png",		// The default value for the setting
+	  type: String,
+	  onChange: updateIcon, // A callback function which triggers when the setting is changed
+	});	
+	
 });
 
 Hooks.on("renderApplication", function() {
   console.log("This code runs when Foundry VTT renders a scene.");
-  updateTitle();
+  updateTab();
 });
+
+function updateTab(){
+	updateTitle();
+	updateIcon();
+}
 
 function updateTitle(){
 	document.title = game.settings.get("update-title", "pagetitle");
+}
+
+function updateIcon(){
+	var link = 	document.querySelector("link[rel='icon']")
+	console.log(link);
 }
