ff1f60ab22cc800b7fc68e2e1dfa0baff6cde02c
Anders Grasdal <WobblyMusicStand@gmail.com>
2021-04-09 20:06:03 -0700

    Updated to use game settings and renderApplication Hooks.

--- update-title.js
+++ update-title.js
@@ -1,10 +1,28 @@
 var
 
 function updateTitle(){
-	document.title = 'New Title!';
+	document.title = game.settings.get("update-title", "pagetitle");
 }
 
-Hooks.on("ready", function() {
-  console.log("This code runs once the Foundry VTT software begins it's initialization workflow.");
+/* Call back after init to include module settings.
+*/
+Hooks.on("init", function() {
+	// Register a client setting
+	game.settings.register("update-title", "pagetitle", {
+	  name: "Page Title",
+	  hint: "The title that appears on the browser tab",
+	  scope: "world",     // This specifies a client-stored setting
+	  config: true,        // This specifies that the setting appears in the configuration view
+	  type: String,
+	  default: " • Foundry Virtual Tabletop",        // The default value for the setting
+	  onChange: value => { // A callback function which triggers when the setting is changed
+		console.log(value)
+	  }
+	});	
+	
+});
+
+Hooks.on("renderApplication", function() {
+  console.log("This code runs when Foundry VTT renders a scene.");
   updateTitle();
 });
